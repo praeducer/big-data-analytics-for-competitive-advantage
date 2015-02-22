c8666ce5c758de94eea6550be6f74973f97b8df7
praeducer <paul.prae@outlook.com>
2015-02-22 21:11:44 +0000

    Now able to print actor data for three pages. Breaks on at least the fourth and fifth it tries to parse because the html formatting changed. See TODO in comments.

--- film-proposal/data-collection/actor_extractor.py
+++ film-proposal/data-collection/actor_extractor.py
@@ -1,3 +1,9 @@
+# TODO:
+#	Handle special cases for html formatting on some of the pages. e.g.:
+#		http://en.wikipedia.org/wiki/18th_Screen_Actors_Guild_Awards
+#		http://en.wikipedia.org/wiki/17th_Screen_Actors_Guild_Awards
+#	Add in a column for which award ceremony it was, e.g. '17th Screen Actors Guild Awards'. Should just be the title of the wikipedia page. Wikipedia library can do this easy.
+
 from bs4 import BeautifulSoup
 import urllib
 import wikipedia
@@ -14,14 +20,29 @@ for item in winnersForYearsATags:
 	if item.has_attr('href'):
 		winnersForYearsURLs.append(wikipediaRoot + item['href']);
 
-winnersForAYearPage = winnersForYearsURLs[0];
-winnersForAYearSoup = BeautifulSoup(urllib.urlopen(winnersForAYearPage));
-filmWinnersForAYearHeader = winnersForAYearSoup.find(id='Film').parent;
-filmWinnersForAYearTable = filmWinnersForAYearHeader.find_next_sibling('table');filmWinnersForAYearRows = filmWinnersForAYearTable.find_all('tr');
-leadActorsColumns = filmWinnersForAYearRows[1].find_all('td');
-leadMaleActorATag = leadActorsColumns[0].find_all('a')[0];
-leadMaleActor = leadMaleActorATag['title'];
-leadMaleActorURL = wikipediaRoot + leadMaleActorATag['href'];
-print(leadMaleActor);
-print(leadMaleActorURL);
+for winnersForAYearPage in winnersForYearsURLs:
+	winnersForAYearSoup = BeautifulSoup(urllib.urlopen(winnersForAYearPage));
+	filmWinnersForAYearHeader = winnersForAYearSoup.find(id='Film').parent;
+	filmWinnersForAYearTable = filmWinnersForAYearHeader.find_next_sibling('table');
+	filmWinnersForAYearRows = filmWinnersForAYearTable.find_all('tr');
+
+	leadActorsColumns = filmWinnersForAYearRows[1].find_all('td');
+	leadMaleActorATag = leadActorsColumns[0].find_all('a')[0];
+	leadMaleActor = leadMaleActorATag['title'];
+	leadMaleActorURL = wikipediaRoot + leadMaleActorATag['href'];
+	print(leadMaleActor + ',' + leadMaleActorURL + ',male,lead');
+	leadFemaleActorATag = leadActorsColumns[1].find_all('a')[0];
+	leadFemaleActor = leadFemaleActorATag['title'];
+	leadFemaleActorURL = wikipediaRoot + leadFemaleActorATag['href'];
+	print(leadFemaleActor + ',' + leadFemaleActorURL + ',female,lead');
+
+	supportActorsColumns = filmWinnersForAYearRows[3].find_all('td');
+	supportMaleActorATag = supportActorsColumns[0].find_all('a')[0];
+	supportMaleActor = supportMaleActorATag['title'];
+	supportMaleActorURL = wikipediaRoot + supportMaleActorATag['href'];
+	print(supportMaleActor + ',' + supportMaleActorURL + ',male,support');
+	supportFemaleActorATag = supportActorsColumns[1].find_all('a')[0];
+	supportFemaleActor = supportFemaleActorATag['title'];
+	supportFemaleActorURL = wikipediaRoot + supportFemaleActorATag['href'];
+	print(supportFemaleActor + ',' + supportFemaleActorURL + ',female,support');
 
