6a040e682eb370bb79bbf5a779af3d432a9315ae
praeducer <paul.prae@outlook.com>
2015-02-22 20:36:36 +0000

    Able to print a single male actor. Now time to extend across all actors for all years.

--- film-proposal/data-collection/actor_extractor.py
+++ film-proposal/data-collection/actor_extractor.py
@@ -2,9 +2,26 @@ from bs4 import BeautifulSoup
 import urllib
 import wikipedia
 
-actorsAwardPage = "http://en.wikipedia.org/wiki/Screen_Actors_Guild_Award";
+wikipediaRoot = 'http://en.wikipedia.org';
+actorsAwardPage = wikipediaRoot + '/wiki/Screen_Actors_Guild_Award';
 actorsAwardSoup = BeautifulSoup(urllib.urlopen(actorsAwardPage));
-listOfWinnersHeader = actorsAwardSoup.find(id="List_of_nominees_and_winners").parent;
-listOfWinners = listOfWinnersHeader.find_next_sibling("ul");
-print(listOfWinners);
+
+winnersForYearsHeader = actorsAwardSoup.find(id='List_of_nominees_and_winners').parent;
+winnersForYears = winnersForYearsHeader.find_next_sibling('ul');
+winnersForYearsATags = winnersForYears.find_all('a');
+winnersForYearsURLs = [];
+for item in winnersForYearsATags:
+	if item.has_attr('href'):
+		winnersForYearsURLs.append(wikipediaRoot + item['href']);
+
+winnersForAYearPage = winnersForYearsURLs[0];
+winnersForAYearSoup = BeautifulSoup(urllib.urlopen(winnersForAYearPage));
+filmWinnersForAYearHeader = winnersForAYearSoup.find(id='Film').parent;
+filmWinnersForAYearTable = filmWinnersForAYearHeader.find_next_sibling('table');filmWinnersForAYearRows = filmWinnersForAYearTable.find_all('tr');
+leadActorsColumns = filmWinnersForAYearRows[1].find_all('td');
+leadMaleActorATag = leadActorsColumns[0].find_all('a')[0];
+leadMaleActor = leadMaleActorATag['title'];
+leadMaleActorURL = wikipediaRoot + leadMaleActorATag['href'];
+print(leadMaleActor);
+print(leadMaleActorURL);
 
