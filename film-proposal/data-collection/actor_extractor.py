b3108ba127f743a11ad854c58ddbe8b80750a862
praeducer <paul.prae@outlook.com>
2015-02-23 02:40:52 +0000

    All lead and supporting actors should be printed out now with some meta data. Next up, write it to a csv.

--- film-proposal/data-collection/actor_extractor.py
+++ film-proposal/data-collection/actor_extractor.py
@@ -12,7 +12,7 @@ wikipediaRoot = 'http://en.wikipedia.org';
 actorsAwardPage = wikipediaRoot + '/wiki/Screen_Actors_Guild_Award';
 actorsAwardSoup = BeautifulSoup(urllib.urlopen(actorsAwardPage));
 
-winnersForYearsHeader = actorsAwardSoup.find(id='List_of_nominees_and_winners').parent;
+winnersForYearsHeader = actorsAwardSoup.find('span', { 'id' : 'List_of_nominees_and_winners'}).parent;
 winnersForYears = winnersForYearsHeader.find_next_sibling('ul');
 winnersForYearsATags = winnersForYears.find_all('a');
 winnersForYearsURLs = [];
@@ -21,28 +21,45 @@ for item in winnersForYearsATags:
 		winnersForYearsURLs.append(wikipediaRoot + item['href']);
 
 for winnersForAYearPage in winnersForYearsURLs:
-	winnersForAYearSoup = BeautifulSoup(urllib.urlopen(winnersForAYearPage));
-	filmWinnersForAYearHeader = winnersForAYearSoup.find(id='Film').parent;
-	filmWinnersForAYearTable = filmWinnersForAYearHeader.find_next_sibling('table');
-	filmWinnersForAYearRows = filmWinnersForAYearTable.find_all('tr');
 
-	leadActorsColumns = filmWinnersForAYearRows[1].find_all('td');
-	leadMaleActorATag = leadActorsColumns[0].find_all('a')[0];
+	winnersForAYearSoup = BeautifulSoup(urllib.urlopen(winnersForAYearPage));
+	winnersForAYearTitle = winnersForAYearSoup.title.contents[0];
+	winnersForAYearTitle = winnersForAYearTitle.replace(' - Wikipedia, the free encyclopedia','');	
+
+	filmWinnersForAYearTable = winnersForAYearSoup.find('table',{ 'class' : 'wikitable'});
+	if filmWinnersForAYearTable is not None:
+		filmWinnersForAYearRows = filmWinnersForAYearTable.find_all('tr');
+
+		leadActorsColumns = filmWinnersForAYearRows[1].find_all('td');
+		leadMaleActorATag = leadActorsColumns[0].find_next('a');
+		leadFemaleActorATag = leadActorsColumns[1].find_next('a');
+
+		supportActorsColumns = filmWinnersForAYearRows[3].find_all('td');
+		supportMaleActorATag = supportActorsColumns[0].find_next('a');
+		supportFemaleActorATag = supportActorsColumns[1].find_next('a');
+	else:	
+		leadMaleActorHeader = winnersForAYearSoup.find('span', { 'id' : 'Outstanding_Performance_by_a_Male_Actor_in_a_Leading_Role'});
+		leadFemaleActorHeader = winnersForAYearSoup.find('span', { 'id' : 'Outstanding_Performance_by_a_Female_Actor_in_a_Leading_Role'});
+		supportMaleActorHeader = winnersForAYearSoup.find('span', { 'id' : 'Outstanding_Performance_by_a_Male_Actor_in_a_Supporting_Role'});
+		supportFemaleActorHeader = winnersForAYearSoup.find('span', { 'id' : 'Outstanding_Performance_by_a_Female_Actor_in_a_Supporting_Role'});
+		leadMaleActorATag = leadMaleActorHeader.find_next('b').find_next('a');
+		leadFemaleActorATag = leadFemaleActorHeader.find_next('b').find_next('a');
+		supportMaleActorATag = supportMaleActorHeader.find_next('b').find_next('a');
+		supportFemaleActorATag = supportFemaleActorHeader.find_next('b').find_next('a');
+
 	leadMaleActor = leadMaleActorATag['title'];
 	leadMaleActorURL = wikipediaRoot + leadMaleActorATag['href'];
-	print(leadMaleActor + ',' + leadMaleActorURL + ',male,lead');
-	leadFemaleActorATag = leadActorsColumns[1].find_all('a')[0];
+	print(leadMaleActor + ',' + leadMaleActorURL + ',male,lead,' + winnersForAYearTitle);
+	
 	leadFemaleActor = leadFemaleActorATag['title'];
 	leadFemaleActorURL = wikipediaRoot + leadFemaleActorATag['href'];
-	print(leadFemaleActor + ',' + leadFemaleActorURL + ',female,lead');
+	print(leadFemaleActor + ',' + leadFemaleActorURL + ',female,lead,' + winnersForAYearTitle);
 
-	supportActorsColumns = filmWinnersForAYearRows[3].find_all('td');
-	supportMaleActorATag = supportActorsColumns[0].find_all('a')[0];
 	supportMaleActor = supportMaleActorATag['title'];
 	supportMaleActorURL = wikipediaRoot + supportMaleActorATag['href'];
-	print(supportMaleActor + ',' + supportMaleActorURL + ',male,support');
-	supportFemaleActorATag = supportActorsColumns[1].find_all('a')[0];
+	print(supportMaleActor + ',' + supportMaleActorURL + ',male,support,' + winnersForAYearTitle);
+
 	supportFemaleActor = supportFemaleActorATag['title'];
 	supportFemaleActorURL = wikipediaRoot + supportFemaleActorATag['href'];
-	print(supportFemaleActor + ',' + supportFemaleActorURL + ',female,support');
+	print(supportFemaleActor + ',' + supportFemaleActorURL + ',female,support,' + winnersForAYearTitle);
 
