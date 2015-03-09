5281119a6cd394b6b929a452b931a177d552b3a2
Daniel Joensen <dantheman340@gmail.com>
2015-03-08 20:07:16 -0400

    updated version

--- film-proposal/data-collection/director_extractor_function.py
+++ film-proposal/data-collection/director_extractor_function.py
@@ -1,6 +1,6 @@
 # description: Accepts a film's URL and outputs the Director from Wikipedia
 # author: Daniel Joensen
-# since: 3/0/2015
+# since: 3/08/2015
 # tested with Python 3.4.2 on windows 7 Ultimate 64 bit
 
 import sys
@@ -9,24 +9,34 @@ import urllib
 import wikipedia
 
 filmURLs = [
-'http://en.wikipedia.org/wiki/Planes_(film)'
+'http://en.wikipedia.org/wiki/Lovin%27_Molly',
+'http://en.wikipedia.org/wiki/Maestro_(manga)',
+'http://en.wikipedia.org/wiki/Manic_(film)',
+'http://en.wikipedia.org/wiki/Max_(2002_film)',
+'http://en.wikipedia.org/wiki/Merlin_(film)'
 ]
 
 for filmURL in filmURLs:
 	def director_extractor(filmURL):
 		filmPageSoup = BeautifulSoup(urllib.request.urlopen(filmURL));
 		summaryTable = filmPageSoup.find('table',{ 'class' : 'infobox vevent'});
-		summaryTableRows = summaryTable.find_all('tr');
-
+		
 		directorNames = [];
 
-		for row in summaryTableRows:
-			if 'Directed' in row.text:
-				directorNameATags = row.find_all('a');
-				#print(directorNameATags);
-				for tag in directorNameATags:
-					directorName = tag['title'];
-					directorNames.append(directorName);
+		try:
+			summaryTableRows = summaryTable.find_all('tr');
+
+			for row in summaryTableRows:
+				if 'Directed' in row.text:
+					directorNameATags = row.find_all('a');
+					for tag in directorNameATags:
+						if tag['href'][0] == "/":			
+							directorName = tag['title'].replace(',','').replace('\'','').replace('"','');
+							directorURL = tag['href'].replace(',','').replace('\'','').replace('"','');
+							directorTuple = {'name': directorName, 'url': directorURL};
+							directorNames.append(directorTuple);
+		except AttributeError:
+			directorNames.append("null");
 
 		return directorNames;
 
