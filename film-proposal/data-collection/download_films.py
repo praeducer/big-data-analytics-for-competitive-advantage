dbdfbf633b8d91fafbc506cd516943133e844f92
praeducer <paul.prae@outlook.com>
2015-03-09 02:56:20 +0000

    added in full list as input. printing out to cmd line so we know where we are at. tested on all films.

--- film-proposal/data-collection/download_films.py
+++ film-proposal/data-collection/download_films.py
@@ -5,13 +5,16 @@ import requests
 filmDataFilename = './data/test_film_urls.csv';
 filmDataReader = csv.reader(open(filmDataFilename));
 filmDataReader.__next__();
+count = 0;
 for filmData in filmDataReader:
+	count += 1;
 	url = filmData[0];
 	urlPieces = url.split('/');
 	pageName = urlPieces[len(urlPieces) - 1];
 	pageFile = open('./data/films/' + pageName + '.html', 'w');
 	try:
 		response = requests.get(url);
+		print(str(count) + '. Writing ' + pageName);
 		pageFile.write(response.text);
 	except requests.exceptions.RequestException:
 		print('Exception: Bad Request for ' + pageName + ' -> ' + url);
