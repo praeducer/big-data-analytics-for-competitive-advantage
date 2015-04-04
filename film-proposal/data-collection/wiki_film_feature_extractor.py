16c1ab0e773753054be956caff34d15e83a65708
Paul Prae <paul.prae@outlook.com>
2015-04-03 21:26:51 -0400

    created a mapper function in prep for hadoop. created a loopLocal function for running outside of hadoop.

--- film-proposal/data-collection/wiki_film_feature_extractor.py
+++ film-proposal/data-collection/wiki_film_feature_extractor.py
@@ -2,6 +2,12 @@
 # authors: Paul Prae, Daniel Joensen
 # since: 3/08/2015
 # tested with Python 3.3 on CentOS 7
+# TODO: Create reducer function
+# TODO: Add in command line parameters
+# --mapper	turns this into a script that acts as just the mapper function.
+# --reducer	turns this into a script that acts as just the reducer function.
+# --stdio	uses STDIN as the input and STDOUT as the output for the map reduce functions. may need to always assume if a map or reduce flas is present this is true.
+# otherwise run this locally with a loop over a list of directory names.
 
 import os
 import sys
@@ -9,6 +15,7 @@ import re
 from bs4 import BeautifulSoup
 import wikipedia
 import csv
+import ntpath
 # custom
 import wiki_feature_extractors
 
@@ -44,59 +51,74 @@ def build_film_index():
 		filmIndex[filmData[0]] = {'title': filmData[1], 'year': filmData[2]};
 	return filmIndex;
 
-if __name__=="__main__":
+def mapper(fullFilmFilePath, filmIndex, stdIO):
+	try:
+		filmPage = open(fullFilmFilePath, encoding="utf8");
+	except IsADirectoryError:
+		return "";
+	filmPageSoup = BeautifulSoup(filmPage.read());
+	filmFileName = ntpath.basename(fullFilmFilePath);
+	filmURLName = os.path.splitext(filmFileName)[0];
+	filmURL = 'http://en.wikipedia.org/wiki/' + filmURLName;
+	
+	filmDate = wiki_feature_extractors.find_release_date(filmPageSoup);
 
-	filmWriter = csv.writer(open('./data/test_film_data.csv', 'w', newline=''));
+	budgetValue = wiki_feature_extractors.find_budget(filmPageSoup);
+	if not budgetValue:
+		budgetValue = 'null';
+	budgetValue = budgetValue.replace(',','');
+
+	revenueValue = wiki_feature_extractors.find_revenue(filmPageSoup);
+
+	directorData = wiki_feature_extractors.find_director(filmPageSoup);
+	directorValue = tuples_to_column_value(directorData);
+
+	actorData = wiki_feature_extractors.find_actor(filmPageSoup);
+	actorValue = tuples_to_column_value(actorData);
+
+	distributionData = wiki_feature_extractors.find_distribution_company(filmPageSoup);
+	distributionValue = tuples_to_column_value(distributionData);
+
+	genreData = wiki_feature_extractors.find_genre(filmPageSoup);
+	genreValue = list_to_column_value(genreData);
+
+	try:
+		filmData = filmIndex.get(filmURL);
+		if filmData:
+			filmTitle = filmData.get('title');
+			if not filmTitle:
+				filmTitle = filmURLName;
+			filmYear = filmData.get('year');
+			if not filmYear:
+				filmYear = 'null';
+		else:
+			filmTitle = 'null';
+			filmYear = 'null';
+		return [filmTitle, filmURL, filmDate, filmYear, budgetValue, revenueValue, directorValue, actorValue, distributionValue, genreValue];
+	except KeyError:
+		return "";
+
+def loopLocal(stdIO, inputDirectory, outputFile):
+	filmWriter = csv.writer(open(outputFile, 'w', newline=''));
 	filmIndex = build_film_index();
 	filmWriter.writerow(['title', 'url', 'release date', 'release year', 'budget', 'revenue', 'director', 'actor', 'distributor', 'genre']);
-	relativeFilmFilePath = './data/films/test/';
-	filmFiles = os.listdir(relativeFilmFilePath);
+	
+	filmFiles = os.listdir(inputDirectory);
 	count = 0;
 	for filmFileName in filmFiles:
 		count += 1;
 		print(str(count) + ' - ' + filmFileName);
-		fullFilmFilePath = relativeFilmFilePath + filmFileName;
-		try:
-			filmPage = open(fullFilmFilePath, encoding="utf8");
-		except IsADirectoryError:
-			pass;
-		filmPageSoup = BeautifulSoup(filmPage.read());
-		filmURLName = os.path.splitext(filmFileName)[0];
-		filmURL = 'http://en.wikipedia.org/wiki/' + filmURLName;
+		fullFilmFilePath = inputDirectory + filmFileName;
+		filmRow = mapper(fullFilmFilePath, filmIndex, stdIO);
+		filmWriter.writerow(filmRow);
+
+if __name__=="__main__":
+
+	# assume we are not in stdIO mode
+	stdIO = False;
+	
+	if not stdIO:
+		inputDirectory = './data/films/test/';
+		outputFile = './data/test_film_data.csv';
+		loopLocal(stdIO, inputDirectory, outputFile);
 		
-		filmDate = wiki_feature_extractors.find_release_date(filmPageSoup);
-
-		budgetValue = wiki_feature_extractors.find_budget(filmPageSoup);
-		if not budgetValue:
-			budgetValue = 'null';
-		budgetValue = budgetValue.replace(',','');
-
-		revenueValue = wiki_feature_extractors.find_revenue(filmPageSoup);
-
-		directorData = wiki_feature_extractors.find_director(filmPageSoup);
-		directorValue = tuples_to_column_value(directorData);
-
-		actorData = wiki_feature_extractors.find_actor(filmPageSoup);
-		actorValue = tuples_to_column_value(actorData);
-
-		distributionData = wiki_feature_extractors.find_distribution_company(filmPageSoup);
-		distributionValue = tuples_to_column_value(distributionData);
-
-		genreData = wiki_feature_extractors.find_genre(filmPageSoup);
-		genreValue = list_to_column_value(genreData);
-
-		try:
-			filmData = filmIndex.get(filmURL);
-			if filmData:
-				filmTitle = filmData.get('title');
-				if not filmTitle:
-					filmTitle = filmFileName;
-				filmYear = filmData.get('year');
-				if not filmYear:
-					filmYear = 'null';
-			else:
-				filmTitle = 'null';
-				filmYear = 'null';
-			filmWriter.writerow([filmTitle, filmURL, filmDate, filmYear, budgetValue, revenueValue, directorValue, actorValue, distributionValue, genreValue]);
-		except KeyError:
-			pass;
