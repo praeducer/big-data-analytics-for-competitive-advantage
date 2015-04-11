ea1069ba83b2215d3bd7d4bde6349ae631c9cfa6
Daniel Joensen <dantheman340@gmail.com>
2015-04-11 00:23:52 -0400

    finished adding all major functions, updated TODO list to final items.

--- film-proposal/data-collection/bom_film_data_extractors.py
+++ film-proposal/data-collection/bom_film_data_extractors.py
@@ -10,12 +10,27 @@ import wikipedia
 import csv
 from bs4 import BeautifulSoup
 
-#TODO: Add movie name and release date functions
+#TODO: Finish integrating the urls from the excel input file
+#TODO: Organize and test final data output
+#TODO: Replace dollar signs and commas in dollar outputs
+#TODO: Reformat date field to convert release date into MM/DD/YYYY, or worst case just extract the year.
 
-movie_ID = 'hobbit3.htm';
+movie_ID = 'godzilla2012.htm';
 base_URL = 'http://www.boxofficemojo.com/movies';
 movie_URL = ('%s/?id=%s' % (base_URL,movie_ID));
 movie_soup = BeautifulSoup(urllib.request.urlopen(movie_URL));
+
+
+def find_movie_name(movie_soup):
+	table_list = movie_soup.find_all('table');
+	target_table = table_list[2];
+	table_rows = target_table.find_all('tr');
+	target_row = table_rows[0];
+	target_columns = target_row.find_all('td', {'align':'center'});
+	target_column = target_columns[1];
+	target_column = target_column.find('b')
+	movie_title = target_column.text;
+	return movie_title;
 
 def find_domestic_total(movie_soup):
 	revenue_table = movie_soup.find('div', {'class':'mp_box_content'});
@@ -95,17 +110,32 @@ def find_mpaa_rating(movie_soup):
 #	center_table_data = center_table_data.text
 	return center_table_data;
 
+def find_release_date(movie_soup):
+	center_table = movie_soup.findChildren('center')[0];
+	center_table_data = center_table.findChildren('tr');
+	for row in center_table_data:
+		if 'Distributor' in row.text:
+			if 'Release' in row.text:
+				release_date = row.find_all('b');
+				release_date = release_date[1].text;
+		else:
+			if 'Release' in row.text:
+				release_date = row.find('b');
+				release_date = release_date.text;
+	return release_date;
+
 domestic_total = find_domestic_total(movie_soup);
 foreign_total = find_foreign_total(movie_soup);
 global_total = find_global_total(movie_soup);
 opening_weekend_total = find_opening_weekend_total(movie_soup);
 number_of_theaters = find_number_of_theaters(movie_soup);
 mpaa_rating = find_mpaa_rating(movie_soup);
+movie_name = find_movie_name(movie_soup);
+release_date = find_release_date(movie_soup);
 
-
+"""
 filename = './data/bom_film_urls.csv';
 bom_film_urls = [];
-
 film_url_reader = csv.reader(open(filename));
 for row in film_url_reader:
 	bom_film_urls.append(row);
@@ -114,6 +144,13 @@ for item in bom_film_urls:
 	print (item);
 
 """
+
+print("Film Title: ");
+print(movie_name);
+
+print("Release Date: ");
+print(release_date);
+
 print("Domestic total: ");
 print(domestic_total);
 
@@ -131,4 +168,3 @@ print(number_of_theaters);
 
 print("MPAA Rating: ");
 print(mpaa_rating);
-"""
