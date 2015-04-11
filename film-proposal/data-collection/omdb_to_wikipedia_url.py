999c22049389d2effe411a9d27d44b27668e7ace
Paul Prae <paul.prae@outlook.com>
2015-04-10 21:02:42 -0400

    performed almost all previous todos. This is working very well now. It is in a more functional style without recursion. It has a confidence score that predicts accuract well.

--- film-proposal/data-collection/omdb_to_wikipedia_url.py
+++ film-proposal/data-collection/omdb_to_wikipedia_url.py
@@ -1,15 +1,14 @@
-# description: takes in a list of movie titles and outputs a list of wikipedia URLs.
+# description: takes in a list of movie titles and outputs a list of wikipedia URLs and a confidence level.
 # input: The Open Movie Database's Database Dump. e.g. https://github.com/praeducer/big-data-analytics-for-competitive-advantage/blob/master/film-proposal/data-collection/data/omdb/test/omdbMovies_tiny_sample.txt
-# file output: a .csv e.g. https://github.com/praeducer/big-data-analytics-for-competitive-advantage/blob/master/film-proposal/data-collection/data/wikipedia_urls.csv
-# cmd line output: e.g. https://github.com/praeducer/big-data-analytics-for-competitive-advantage/blob/master/film-proposal/data-collection/data/cmd_output.txt
+# file output: a .csv e.g. https://github.com/praeducer/big-data-analytics-for-competitive-advantage/blob/master/film-proposal/data-collection/data/omdb/test/omdb_movies_wikipedia_urls_tiny_sample.csv
+# cmd line output: e.g. https://github.com/praeducer/big-data-analytics-for-competitive-advantage/blob/master/film-proposal/data-collection/data/omdb/test/omdb_to_url_cmd_output.txt
 # author: Paul Prae
 # since: 3/3/2015
 # tested with Python 3.3 on CentOS 7 and Windows 8.1
-# TODO: FIX RECURSIVE MADNESS
 # TODO: Unit Test all strange cases. Will help this make more sense to others too.
-# TODO: Rethink recursion for readability. 
-# TODO: Way too many inputs to these functions. Simplify.
-# TODO: This script needs to figure out if its OO or functional.
+# TODO: Optimize this. It can be slow over many records.
+# TODO: 'Using slow pure-python SequenceMatcher. Install python-Levenshtein to remove this warning'
+# TODO: Make confidence level more scientific. 
 
 import sys
 import csv
@@ -21,78 +20,117 @@ import difflib
 from fuzzywuzzy import fuzz
 
 # returns a tuple of a url and its quality score.
-# TODO: Make it explicitly separate to search a query directly versus its best match.
-def bestURLSearch(queries, title):
-	bestURLs = {}
-	# get first query
-	query = next (iter (queries.keys()))
-	# Use recursion instead of for loop since queries changes over time.
-	while query:
-		printQuery(query, 'Searching best match: ')
-		searchResults = wikipedia.search(query)
-		if searchResults:
-			bestURL, queries = findBestURL(query, queries, searchResults, title)
-			if bestURL:
-				bestURLs[bestURL[0]] = bestURL[1]
-		# get next query. returns false if we are done.
-		query = getNextQuery(queries)
-	if bestURLs:
-		bestURL = max(bestURLs.items(), key=operator.itemgetter(1))[0]
-		return (bestURL, bestURLs[bestURL])
-	return ()
+def bestURLSearch(title, year):
+	bestURLs = []
+	queries = []
 
-def getNextQuery(queries):
-	for query, beenQueried in queries.items():
-		# If there are queries left to be queried.
-		if not beenQueried:
-			return query
-	# If we make it to here, no more left.
-	return False
 
-# returns ((url, score), queries)
-def findBestURL(query, queries, options, title):
-	bestMatch = difflib.get_close_matches(query, options, 1)
-	if bestMatch:
-		# Make sure we have not already processed bestMatch before setting it to zero.
-		if not bestMatch[0] in queries.keys():
-			# Add best match to query list
-			queries[bestMatch[0]] = 0
-		# Because the match was recommended this guarantees a result.
-		return findPageURL(bestMatch[0], queries, title)
-	# 1 means true, it has been queried
-	elif queries[query]:
-		for option in options:
-			print('\toption: ' + option)
-			if 'film)' in option:
-				return findPageURL(option, queries, title)
-	else:
-		print("\t! - No good match from search results. Using raw query.")
-		# No guarantee we will find anything.
-		return findPageURL(query, queries, title)
-	return ((), queries)
+	# Start with two queries: one with (film) and one without. Run both and see which fuzzy score is better.
+	titleWithFilm = title + ' (film)'
+	# get wikipedia's best guess for both. appends (url, score). if nothing was found ('N/A', 0)
+	bestURLs.append(findPageURL(title, title, year))
+	queries.append(title)
+	bestURLs.append(findPageURL(titleWithFilm, title, year))
+	queries.append(titleWithFilm)
 
-# query param: (query, <0 or 1 depending if searched yet or not)
-# returns ((url, score), queries)
-def findPageURL(query, queries, title):
-	# Do not find a page for a query if its been searched for already. Prevents redundant recursion.
-	# 0 means it has not been queried. 
-	if not queries[query]:
-		printQuery(query, 'Finding page for: ')
-		# mark as searched since we actually tried to find a page with it.
-		queries[query] = 1
-		try:
-			page = wikipedia.page(query)
-			if page:
-				# first query is always the title we are looking for
-				pageScore = scorePage( title, page)
-				return ((page.url, pageScore), queries)
-		except wikipedia.exceptions.PageError:
-			pass
-		except wikipedia.exceptions.DisambiguationError as e:
-			print("\t! - DisambiguationError: Searching through other options.")
-			return findBestURL(query, queries, e.options, title)
-	return ((), queries)
+	# Lazy optimization
+	# NOTE: Don't do this unless you need to reduce processing time.
+	# After some testing, this means about a perfect match
+	desiredScore = 0.875
+	currentBestURL, currentBestScore = max(bestURLs, key=operator.itemgetter(1))
+	if currentBestScore >= desiredScore:
+		return (currentBestURL, round(currentBestScore, 2))
 
+	# get best options from a search with each
+	titleBestOption = findBestOption(title, title)
+	titleWithFilmBestOption = findBestOption(titleWithFilm, title)
+
+	# search best options if they are not the same as queries we already searched for
+	if titleBestOption and titleBestOption not in queries:
+		bestURLs.append(findPageURL(titleBestOption, title, year))
+		queries.append(titleBestOption)
+	if titleWithFilmBestOption and titleWithFilmBestOption not in queries:
+		bestURLs.append(findPageURL(titleWithFilmBestOption, title, year))
+		queries.append(titleWithFilmBestOption)
+
+	# there are some cases where the option has a film version
+	if titleBestOption:
+		titleBestOptionWithFilm = titleBestOption + ' (film)'
+		if titleBestOptionWithFilm not in queries:
+			bestURLs.append(findPageURL(titleBestOptionWithFilm, title, year))
+			#queries.append(titleBestOption)
+
+	url, score = max(bestURLs, key=operator.itemgetter(1))
+	return (url, round(score, 2))
+
+# returns the best query to search
+def findBestOption(query, title):
+	safePrint('\tFinding best option for: ' + query)
+	searchResults = wikipedia.search(query)
+	if searchResults:
+		bestMatch = difflib.get_close_matches(title, searchResults, 1)
+		if bestMatch:
+			return bestMatch[0]
+		else:
+			# do our best
+			for option in searchResults:
+				if 'film' in option or title in option:
+					return option
+			# if we found nothing at all similar in the options, trust wikipedia search and return the first result.
+			return searchResults[0]
+	return None
+
+# returns (url, score)
+def findPageURL(query, title, year):
+	safePrint('\tFinding page for: ' + query)
+	try:
+		page = wikipedia.page(query)
+		if page:
+			pageScore = scorePage(page, title, year)
+			# we made it! we at least have one to work with.
+			return (page.url, pageScore)
+	except wikipedia.exceptions.PageError:
+		# nothing good will come of this. wikipedia thought nothing at all like this exists.
+		pass
+	except wikipedia.exceptions.DisambiguationError as e:
+		# this would provide a list of options to search through (i.e. 'e.options') but our findBestOption function will find those.
+		pass
+	return ('N/A', 0)
+
+# Just return the highest score. This function can simply score a given page.
+# TODO: Find a way to make the bonus scores less arbitrary.
+# TODO: Make confidence more scientific. 
+def scorePage(page, title, year):
+	defaultBonus = 10
+	bonuses = []
+	bonusToScore = {'filmInTitle': defaultBonus, 'filmInSummary': 20, 'yearInTitle': defaultBonus, 'yearInSummary': 20}
+	pageScore = 0
+	pageScore = max( fuzz.partial_ratio(title, page.summary), fuzz.ratio(title, page.title))
+
+	if 'film' in page.title:
+		bonuses.append('filmInTitle')
+		pageScore += bonusToScore['filmInTitle']
+
+	if 'film' in page.summary:
+		bonuses.append('filmInSummary')
+		pageScore += bonusToScore['filmInSummary']
+
+	if str(year) in page.title:
+		bonuses.append('yearInTitle')
+		pageScore += bonusToScore['yearInTitle']
+
+	if str(year) in page.summary:
+		bonuses.append('yearInSummary')
+		pageScore += bonusToScore['yearInSummary']
+
+	pageConfidence = pageScore / (100 + sum(bonusToScore.values()))
+
+	safePrint('\t\tPage Title-> ' + page.title)
+	print('\t\tPage Bonus-> ' + ','.join(bonuses))
+	safePrint('\t\tConfidence-> ' + str(pageConfidence))
+	return pageConfidence
+
+# not currently used but could be useful
 def isTitleSimilarToPage(title, page, targetRatio):
 	estimateTitleIsInSummary = fuzz.partial_ratio(title, page.summary)
 	estimateTitleIsSimilar = fuzz.ratio(title, page.title)
@@ -102,17 +140,12 @@ def isTitleSimilarToPage(title, page, targetRatio):
 		print("\t! - This Wikipedia's page's title, " + str(estimateTitleIsSimilar) + ", and summary, " + str(estimateTitleIsInSummary) + ", were not similar to OMDB title.")
 		return False
 
-# Just return the highest score. This function can simply score a given page.
-# TODO: if the title of the page has the exact title we are searching for plus (film) then give it a bonus.
-# TODO: if the title of the page has the year we are looking for, provide another bonus.
-def scorePage(title, page):
-	return max( fuzz.partial_ratio(title, page.summary), fuzz.ratio(title, page.title))
-
-def printQuery(query, message):
+# a little redundant but handles that exception at least (happens on Windows)
+def safePrint(message):
 	try:
-		print('\t' + message + query)
+		print(message)
 	except UnicodeEncodeError:
-		print('\tquery: <UnicodeEncodeError during print. Search is fine.>')
+		print('\tsafePrint: <UnicodeEncodeError during print. Search is fine.>')
 
 if __name__=="__main__":
 	outputFilename = './data/omdb/test/omdb_movies_wikipedia_urls_tiny_sample.csv'
@@ -127,14 +160,12 @@ if __name__=="__main__":
 		lineCount += 1
 		print(' ~ ' + str(lineCount) + ' ~ ')
 		title = line[2].strip('\n\r')
-		titleFilm = title + ' (film)'
-		queries = {title: 0, titleFilm: 0}
-		# Start with two queries: one with (film) and one without. Run both and see which fuzzy score is better.
+		year = line[3]
 		# Highest scoring page found is the one we use the URL from. Save URL and score. N/A only if it was impossible to find any page at all.
-		url = bestURLSearch(queries, title)
+		url = bestURLSearch(title, year)
 		if not url:
 			print("\t! - No URL found")
 			url = ('N/A', 0)
 		print('\tresult-> ' + url[0] + ", " + str(url[1]))
-		line.append(url)
+		line.extend(url)
 		filmDataWriter.writerow(line)
