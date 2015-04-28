abf7937585403793f2570f07ac43abf62713e9f3
Paul Prae <paul.prae@outlook.com>
2015-04-28 08:09:17 -0400

    added in newest version of the r model. cleaned it up a bit. still need to talk with Ben before testing.

--- film-proposal/prediction/random-forest-predictor.r
+++ film-proposal/prediction/random-forest-predictor.r
@@ -1,5 +1,6 @@
 # description: predict the revenue of films based on aspects of production using http://en.wikipedia.org/wiki/Random_forest.
-# author: 
+# author: Ben Adelman (Original), Paul Prae
+# citation: inspired by a tutorial for a Kaggle competition written by Trevor Stephens: https://github.com/trevorstephens/titanic
 
 library(rpart)
 install.packages('rattle')
@@ -14,39 +15,32 @@ install.packages('party')
 library(party)
 
 ##  load the full file into R
-full_data <- read.csv("./data/test_film_data.csv")
+full_data <- read.csv("./data/analysis_data.csv")
+## possible_data <- read.csv("./data/possible_films.csv")
 
-## trying to handle/correct some data types
-full_data$Release_Date <- as.Date(full_data$Release_Date, "%m/%d/%Y")
-full_data$Budget <- as.numeric(as.character(full_data$Budget))
-full_data$Revenue <- as.numeric(as.character(full_data$Revenue))
+## adjusting some data types
+full_data$Adj_Budget <- as.numeric(as.character(full_data$Adj_Budget))
+full_data$Adj_Revenue <- as.numeric(as.character(full_data$Adj_Rev))
+full_data$Adj_Weekend_Total <- as.numeric(as.character(full_data$Adj_Revenue))
 
-##  now let's split into test vs. training data
-##  75% of the sample size for train
+##  75% of the sample size for train (used for evaluation run, not final run)
 smp_size <- floor(0.75 * nrow(full_data))
 
-## set the seed to make your partition reproductible (a random number seed)
+## set the seed to make your partition reproducible (a random number seed)
 set.seed(123)
 train_ind <- sample(seq_len(nrow(full_data)), size = smp_size)
 
+##  now let's split into test vs. training data
 train <- full_data[train_ind, ]
 test <- full_data[-train_ind, ]
 
-##  set up the random forest model for predicting ROI
-#fit_ROI <- randomForest(as.factor(ROI) ~ release_year + Budget + Revenue + Release.Day.of.Week + Notable_Actor + Notable_Director + Notable_Production_co + Genre_comedy + Genre_drama, data=train, importance=TRUE, ntree=2000)
-
-##  set up the random forest model for predicting Total Sales 
-fit_Sales <- randomForest(as.factor(Revenue) ~ release_year + Budget + Release.in.Winter + Release.Day.of.Week + Release.in.Spring + Release.in.Summer + Release.in.Fall + Release.in.Holiday + Notable_Actor + Notable_Director + Notable_Production_co + Genre_comedy + Genre_drama, data=train, importance=TRUE, ntree=2000)
-
-##  set up the random forest model for predicting Opening Weekend
-#fit_OpenWkend <- randomForest(as.factor(Opening_Weekend) ~ release_year + Budget + Revenue + Release.in.Winter + Release.Day.of.Week + Release.in.Spring + Release.in.Summer + Release.in.Fall + Release.in.Holiday + Notable_Actor + Notable_Director + Notable_Production_co + Genre_comedy + Genre_drama, data=train, importance=TRUE, ntree=2000)
+##  set up the random forest model for predicting Total Revenue 
+fit_Revenue <- randomForest(as.factor(Adj_Revenue) ~ Adj_Budget + MPAA_Rating + Release_in_Winter + Release_in_Spring + Release_in_Summer + Release_in_Fall + Release_in_Holiday + Actor_Morgan_Freeman + Actor_Dennis_Hopper + Actor_Henry_Fonda + Actor_Bruce_Willis + Actor_Samuel_L__Jackson + Actor_Robert_De_Niro + Actor_Burt_Lancaster + Actor_Donald_Sutherland + Actor_Christopher_Lee + Actor_John_Wayne + Actor_Keanu_Reeves + Actor_Nick_Nolte + Actor_Nicolas_Cage + Actor_Gene_Hackman + Actor_Michael_Caine + Actor_Sean_Connery + Actor_Oliver_Hardy + Actor_Stan_Laurel + Actor_Robert_Duvall + Actor_Susan_Sarandon + Actor_Jack_Nicholson + Actor_Robert_Downey_Jr_ + Actor_Christopher_Walken + Actor_Willem_Dafoe + Actor_James_Stewart + Actor_Dustin_Hoffman + Actor_Robin_Williams + Actor_John_Goodman + Actor_Dennis_Quaid + Actor_Harvey_Keitel + Director_Blake_Edwards + Director_Sidney_Lumet + Director_Steven_Spielberg + Director_Spike_Lee + Director_John_Ford + Director_Robert_Altman + Director_Charlie_Chaplin + Director_Vincente_Minnelli + Director_Woody_Allen + Director_Clint_Eastwood + Director_Martin_Scorsese + Director_Ingmar_Bergman + Director_Howard_Hawks + Director_John_Huston + Director_Raoul_Walsh + Director_Chuck_Jones + Director_Werner_Herzog + Director_Fritz_Lang + Director_Steven_Soderbergh + Director_Michael_Curtiz + Director_Francis_Ford_Coppola + Director_Roger_Corman + Director_Alfred_Hitchcock + Director_Friz_Freleng + Director_Anthony_Mann + Director_Norman_Taurog + Director_Akira_Kurosawa + Genre_comedy + Genre_drama + Genre_romantic + Genre_science_fiction + Genre_crime + Genre_action + Genre_thriller + Genre_horror + Genre_animated, data=train, importance=TRUE, ntree=2000)
 
 ##  Now to make the prediction... using the model against the test data...
-#PredictionROI <- predict(fit_ROI, test, OOB=TRUE, type = "response")
-#test$ROIPrediction <- PredictionROI
-
-PredictionRevenue <- predict(fit_Sales, test, OOB=TRUE, type = "response")
-test$PredictedRevenue <- PredictionRevenue
+##  varImpPlot(fit_Revenue)
+PredictionRevenue <- predict(fit_Revenue, test, OOB=TRUE, type = "response")
+test$RevenuePrediction <- PredictionRevenue
 
 ## submit <- data.frame(Row.name = test$row.names, ROI = Prediction)
-write.csv(test, file = "PredictedRevenue.csv", row.names = TRUE)
+write.csv(test, file = "./data/test_predictions.csv", row.names = TRUE)
