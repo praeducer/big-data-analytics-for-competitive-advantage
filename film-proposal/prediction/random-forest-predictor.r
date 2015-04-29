7c4d443bbbe3ac736bc45490521495d4cad75cf5
Paul Prae <paul.prae@outlook.com>
2015-04-29 07:08:00 -0400

    updated file names and removed comments.

--- film-proposal/prediction/random-forest-predictor.r
+++ film-proposal/prediction/random-forest-predictor.r
@@ -1,5 +1,6 @@
 # description: predict the revenue of films based on aspects of production using http://en.wikipedia.org/wiki/Random_forest.
 # author: Ben Adelman (Original), Paul Prae
+# since: 4/10/2015
 # citation: inspired by a tutorial for a Kaggle competition written by Trevor Stephens: https://github.com/trevorstephens/titanic
 
 library(rpart)
@@ -15,8 +16,8 @@ install.packages('party')
 library(party)
 
 ##  load the full file into R
-full_data <- read.csv("./data/analysis_data.csv")
-## possible_data <- read.csv("./data/possible_films.csv")
+full_data <- read.csv("./data/test_film_data_sample.csv")
+#possible_data <- read.csv("./data/possible_films.csv")
 
 ## adjusting some data types
 full_data$Adj_Budget <- as.numeric(as.character(full_data$Adj_Budget))
@@ -38,9 +39,8 @@ test <- full_data[-train_ind, ]
 fit_Revenue <- randomForest(as.factor(Adj_Revenue) ~ Adj_Budget + MPAA_Rating + Release_in_Winter + Release_in_Spring + Release_in_Summer + Release_in_Fall + Release_in_Holiday + Actor_Morgan_Freeman + Actor_Dennis_Hopper + Actor_Henry_Fonda + Actor_Bruce_Willis + Actor_Samuel_L__Jackson + Actor_Robert_De_Niro + Actor_Burt_Lancaster + Actor_Donald_Sutherland + Actor_Christopher_Lee + Actor_John_Wayne + Actor_Keanu_Reeves + Actor_Nick_Nolte + Actor_Nicolas_Cage + Actor_Gene_Hackman + Actor_Michael_Caine + Actor_Sean_Connery + Actor_Oliver_Hardy + Actor_Stan_Laurel + Actor_Robert_Duvall + Actor_Susan_Sarandon + Actor_Jack_Nicholson + Actor_Robert_Downey_Jr_ + Actor_Christopher_Walken + Actor_Willem_Dafoe + Actor_James_Stewart + Actor_Dustin_Hoffman + Actor_Robin_Williams + Actor_John_Goodman + Actor_Dennis_Quaid + Actor_Harvey_Keitel + Director_Blake_Edwards + Director_Sidney_Lumet + Director_Steven_Spielberg + Director_Spike_Lee + Director_John_Ford + Director_Robert_Altman + Director_Charlie_Chaplin + Director_Vincente_Minnelli + Director_Woody_Allen + Director_Clint_Eastwood + Director_Martin_Scorsese + Director_Ingmar_Bergman + Director_Howard_Hawks + Director_John_Huston + Director_Raoul_Walsh + Director_Chuck_Jones + Director_Werner_Herzog + Director_Fritz_Lang + Director_Steven_Soderbergh + Director_Michael_Curtiz + Director_Francis_Ford_Coppola + Director_Roger_Corman + Director_Alfred_Hitchcock + Director_Friz_Freleng + Director_Anthony_Mann + Director_Norman_Taurog + Director_Akira_Kurosawa + Genre_comedy + Genre_drama + Genre_romantic + Genre_science_fiction + Genre_crime + Genre_action + Genre_thriller + Genre_horror + Genre_animated, data=train, importance=TRUE, ntree=2000)
 
 ##  Now to make the prediction... using the model against the test data...
-##  varImpPlot(fit_Revenue)
+varImpPlot(fit_Revenue)
 PredictionRevenue <- predict(fit_Revenue, test, OOB=TRUE, type = "response")
 test$RevenuePrediction <- PredictionRevenue
 
-## submit <- data.frame(Row.name = test$row.names, ROI = Prediction)
 write.csv(test, file = "./data/test_predictions.csv", row.names = TRUE)
